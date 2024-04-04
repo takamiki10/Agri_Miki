dfd58a2f3ff312fb6e044005d11b151c26c5a6fa
takamiki10 <135207458+takamiki10@users.noreply.github.com>
2024-04-04 10:26:05 +0900

    Update yaw_pitch_test.py

--- yaw_pitch_test.py
+++ yaw_pitch_test.py
@@ -1,44 +1,61 @@
 #!/usr/bin/env python3
+
 import os
 import rospy
 from dynamixel_sdk import *
+from dynamixel_sdk_examples.srv import *
+from dynamixel_sdk_examples.msg import *
+
+# Import for Windows or Linux
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
 # Control table address
-ADDR_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
+ADDR_TORQUE_ENABLE      = 64
 ADDR_GOAL_POSITION      = 116
 ADDR_PRESENT_POSITION   = 132
-
 # Protocol version
-PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
-
+PROTOCOL_VERSION        = 2.0
 # Default setting
-BAUDRATE                    = 1000000            # Dynamixel default baudrate : 1000000
-DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-TORQUE_ENABLE               = 1                 # Value for enabling the torque
-TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MAXIMUM_POSITION_VALUE  = 4095              # Maximum position value
+BAUDRATE                = 1000000
+DEVICENAME              = '/dev/ttyUSB0'
+TORQUE_ENABLE           = 1
+TORQUE_DISABLE          = 0
+DXL_MIN_POSITION_VALUE  = 0
+DXL_MAX_POSITION_VALUE  = 4095
+
+
+current_positions = {}
 
 portHandler = PortHandler(DEVICENAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
 
-pos1 = 0
-pos2 = 0
-
 def set_goal_pos_callback(data):
-    global pos1, pos2
+    global current_positions
     print("Set Goal Position of ID %s = %s" % (data.id, data.position))
-    if data.id == 1:
-        pos1 += data.position
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, data.id, ADDR_GOAL_POSITION, pos1 % (DXL_MAXIMUM_POSITION_VALUE + 1))
-    elif data.id == 2:
-        pos2 += data.position
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, data.id, ADDR_GOAL_POSITION, pos2 % (DXL_MAXIMUM_POSITION_VALUE + 1))
-    else:
-        print("Invalid Dynamixel ID:", data.id)
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, data.id, ADDR_GOAL_POSITION, data.position)
+    if dxl_comm_result == COMM_SUCCESS:
+        current_positions[data.id] = data.position
 
 def get_present_pos(req):
+    global current_positions
     dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, req.id, ADDR_PRESENT_POSITION)
-    print("Present Position of ID %s = %s" % (req.id, dxl_present_position))
+    if dxl_comm_result == COMM_SUCCESS:
+        current_positions[req.id] = dxl_present_position
+        print("Present Position of ID %s = %s" % (req.id, dxl_present_position))
     return dxl_present_position
 
 def read_write_py_node():
@@ -48,84 +65,65 @@ def read_write_py_node():
     rospy.spin()
 
 def main():
-    global pos1, pos2
+    global DEVICENAME
+    global current_positions
 
-    # Initialize positions of IDs 1 and 2 to 0
     try:
-        portHandler.openPort()
-        print("Succeeded to open the port")
+       portHandler.openPort()
+       print("Succeeded to open the port")
+    except:
+        print("Failed to open the port")
+        print("Press any key to terminate...")
+        getch()
+        quit()
 
+    try:
         portHandler.setBaudRate(BAUDRATE)
         print("Succeeded to change the baudrate")
+    except:
+        print("Failed to change the baudrate")
+        print("Press any key to terminate...")
+        getch()
+        quit()
 
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, 1, ADDR_GOAL_POSITION, 0)
+    for dxl_id in range(1, 3): 
+        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, dxl_id, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
         if dxl_comm_result != COMM_SUCCESS:
-            print("Failed to initialize position for ID 1: %s" % packetHandler.getTxRxResult(dxl_comm_result))
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+            print("Press any key to terminate...")
+            getch()
+            quit()
         elif dxl_error != 0:
-            print("Failed to initialize position for ID 1: %s" % packetHandler.getRxPacketError(dxl_error))
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+            print("Press any key to terminate...")
+            getch()
+            quit()
+        else:
+            print("DYNAMIXEL ID %d has been successfully connected" % dxl_id)
+        current_positions[dxl_id] = 0 
 
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, 2, ADDR_GOAL_POSITION, 0)
-        if dxl_comm_result != COMM_SUCCESS:
-            print("Failed to initialize position for ID 2: %s" % packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("Failed to initialize position for ID 2: %s" % packetHandler.getRxPacketError(dxl_error))
+    print("Ready to get & set Position.")
 
-        print("Ready to get & set Position.")
+    while True:
+        try:
+            dxl_id = int(input("Enter Dynamixel ID (1 or 2): "))
+            if dxl_id not in current_positions:
+                print("Invalid ID. Please enter 1 or 2.")
+                continue
 
-        while True:
-            try:
-                DXL_ID = int(input("Enter Dynamixel ID: "))
-                position = int(input("Enter desired position for Dynamixel: "))
-                # Update total rotations
-                if DXL_ID == 1:
-                    pos1 += position
-                    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, pos1 % (DXL_MAXIMUM_POSITION_VALUE + 1))
-                elif DXL_ID == 2:
-                    pos2 += position
-                    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, pos2 % (DXL_MAXIMUM_POSITION_VALUE + 1))
-                else:
-                    print("Invalid Dynamixel ID:", DXL_ID)
-                    break
+            position = int(input("Enter desired position for Dynamixel (in degrees): "))
+            dxl_position = int((position / 360) * (DXL_MAX_POSITION_VALUE - DXL_MIN_POSITION_VALUE) + DXL_MIN_POSITION_VALUE)
+            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, dxl_id, ADDR_GOAL_POSITION, dxl_position)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-                if dxl_comm_result != COMM_SUCCESS:
-                    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-                elif dxl_error != 0:
-                    print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-                while True:
-                    try:
-                        other_id = input("Enter other Dynamixel ID (press q to quit): ")
-                        if other_id == 'q':
-                            break
-                        other_id = int(other_id)
-                        other_position = int(input("Enter desired position for other Dynamixel: "))
-                        # Update total rotations
-                        if other_id == 1:
-                            pos1 += other_position
-                            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, other_id, ADDR_GOAL_POSITION, pos1 % (DXL_MAXIMUM_POSITION_VALUE + 1))
-                        elif other_id == 2:
-                            pos2 += other_position
-                            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, other_id, ADDR_GOAL_POSITION, pos2 % (DXL_MAXIMUM_POSITION_VALUE + 1))
-                        else:
-                            print("Invalid Dynamixel ID:", other_id)
-                            break
-                        
-                        if dxl_comm_result != COMM_SUCCESS:
-                            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-                        elif dxl_error != 0:
-                            print("%s" % packetHandler.getRxPacketError(dxl_error))
-                    except KeyboardInterrupt:
-                        print("\nExiting...")
-                        break
-            except KeyboardInterrupt:
-                print("\nExiting...")
-                break
-
-    except Exception as e:
-        print("An error occurred:", e)
-    finally:
-        portHandler.closePort()
-
+        except ValueError:
+            print("Invalid input. Please enter a valid ID and position.")
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            break
 
 if __name__ == '__main__':
     main()
