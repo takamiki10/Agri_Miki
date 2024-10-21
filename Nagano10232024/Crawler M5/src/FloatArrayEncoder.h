204400a53269f636e53b6e0c92c16fd93eef3aa9
takamiki10 <135207458+takamiki10@users.noreply.github.com>
2024-10-21 15:57:04 +0900

    Update FloatArrayEncoder.h

--- Nagano10232024/Crawler M5/src/FloatArrayEncoder.h
+++ Nagano10232024/Crawler M5/src/FloatArrayEncoder.h
@@ -3,18 +3,13 @@
 
 #include <Arduino.h>
 
-#define MOTOR_NUM 3  // We are working with 3 floats
+#define MOTOR_NUM 3
 
 class FloatArrayEncoder {
 public:
-    // Constructor
     FloatArrayEncoder();
-
-    // Encode 3 float values into a byte array
     void encode(float* float_data, uint8_t* encoded_data);
-
-    // Decode a byte array into 3 float values
     void decode(uint8_t* encoded_data, float* data);
 };
 
-#endif // FLOAT_ARRAY_ENCODER_H
+#endif
